d95a4eece35a713b4619998bae05ad5177af03ec
NiVanc <nico@izebra.be>
2020-04-04 20:52:03 +0200

    Understand Vue Cli ENV variables

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{ url }}</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -89,6 +90,11 @@
 <script>
 export default {
   name: "HelloWorld",
+  data() {
+    return {
+      url: process.env.VUE_APP_URL
+    };
+  },
   props: {
     msg: String
   }
